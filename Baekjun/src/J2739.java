c251d29ae812b0eefdd8864a7f05fb72861f5c97
Lamia9304 <lamia9304@naver.com>
2022-03-06 02:10:19 +0900

    구구단

---
+++ Baekjun/src/J2739.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class J2739 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		
+		
+		for(int j=1;j<=9;j++) {
+				System.out.println(a+" * "+j+" = "+a*j);
+			}
+		
+	}
+
+}
