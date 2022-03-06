1fc71cb7068f5802ebc78c2a61eeab86ba9ac578
Lamia9304 <lamia9304@naver.com>
2022-03-06 12:28:55 +0900

    알람 시계 - 리팩토링

--- Baekjun/src/J2884.java
+++ Baekjun/src/J2884.java
@@ -5,17 +5,23 @@ public class J2884 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	 Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		
+		int H = sc.nextInt(); // 시
+		int M = sc.nextInt(); // 분
+		sc.close();
+		
+		if(M < 45) {
+			H--;		// 시(hour) 1 감소
+			M= 60 - (45 - M); 	// 분(min) 감소
+			if(H < 0) {
+				H = 23;
+			}
+			System.out.println(H + " " + M);
+		}
+		else {
+			System.out.println(H + " " + (M - 45));
 	 
-	 int h=sc.nextInt();
-	 int m=sc.nextInt();
-	 
-	 
-	 if(h-1<0&&m-45<=0) {System.out.println(h+23+" "+(m+15));return;}
-	 else if(m-45<=0) System.out.println(h-1+ " "+(m+15));
-	 else System.out.println(h+" " +(m-45));
-	 
-	 
+		}
 	}
-
 }
